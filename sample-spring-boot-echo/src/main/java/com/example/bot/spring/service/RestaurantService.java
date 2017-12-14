66c4063af75cff7468152b9b76ebb9a2a96b7d41
brianlin <brianlin>
2017-12-14 16:09:31 +0800

    place api -6

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/RestaurantService.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/RestaurantService.java
@@ -36,7 +36,6 @@ public class RestaurantService {
             Param.name("type").value("food"),
             Param.name("language").value("zh-TW"));
     List<CarouselColumn> carusels = new ArrayList<CarouselColumn>();
-    String mapUrl = "https://www.google.com/maps/search/?api=1&query= &query_place_id=";
     String photoUrl =
         "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key="
             + apiKey
@@ -50,13 +49,25 @@ public class RestaurantService {
       String imageUrl =
           photoUrl + json.getJSONArray("photos").getJSONObject(0).getString("photo_reference");
       if (carusels.size() == 5) break;
-      String detail = "店名:" + p.getName() + "\n" + "營業狀態:" + p.getStatus().toString();
+      String detail =
+          "店名:"
+              + p.getName()
+              + "\r\n"
+              + "評分"
+              + p.getRating()
+              + "\r\n"
+              + "營業狀態:"
+              + p.getStatus().toString();
       CarouselColumn temp =
           new CarouselColumn(
               imageUrl,
               detail,
               "地址:" + p.getVicinity(),
-              Arrays.asList(new URIAction("map位置點我", mapUrl + p.getPlaceId())));
+              Arrays.asList(
+                  new URIAction(
+                      "map位置點我",
+                      "https://www.google.com/maps/search/?api=1&query= &query_place_id="
+                          + p.getPlaceId())));
       carusels.add(temp);
     }
     TemplateMessage templateMessage = new TemplateMessage("位置找餐廳", new CarouselTemplate(carusels));
