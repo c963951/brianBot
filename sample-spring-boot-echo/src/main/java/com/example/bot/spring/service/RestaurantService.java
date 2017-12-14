efd4d4937fb2f2ec42d25015d9b88c9f028edc38
brianlin <brianlin>
2017-12-14 11:14:55 +0800

    google place -1

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/RestaurantService.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/RestaurantService.java
@@ -18,46 +18,52 @@ import se.walkercrou.places.Place;
 
 public class RestaurantService {
 
-    private static RestaurantService instance = new RestaurantService();
+  private static RestaurantService instance = new RestaurantService();
 
-    public static RestaurantService getInstance() {
-        return instance;
-    }
+  public static RestaurantService getInstance() {
+    return instance;
+  }
 
-    public RestaurantService() {}
+  public RestaurantService() {}
 
-    public TemplateMessage getRestaurant(double lat, double lng) {
-        String apiKey = "AIzaSyB6t-XO4BEyDh1jBzHmeZn5hVB0WQkZLe8";
-        Param[] params = new Param[2];
-        Param b = new Param("type");
-        b.value("restaurant");
-        Param a = new Param("language");
-        a.value("zh-TW");
-        params[0] = a;
-        params[1] = b;
-        GooglePlaces client = new GooglePlaces(apiKey);
-        List<Place> places = client.getNearbyPlaces(lat, lng, 500, params);
-        List<CarouselColumn> carusels = new ArrayList<CarouselColumn>();
-        String photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key=" + apiKey
-                + "&photoreference=";
-        for (int i = 1; i < 6; i++) {
-            String imageUrl = photoUrl;
-            for (Photo p : places.get(i).getPhotos()) {
-                imageUrl = imageUrl + p.getReference();
-                System.out.println(places.get(i).getPhotos().size());
-                if(StringUtils.isEmpty(p.getReference())) {
-                    continue;
-                }
-                break;
-            }
-
-            CarouselColumn temp = new CarouselColumn(imageUrl, Double.toString(places.get(i).getRating()),
-                    places.get(i).getVicinity(),
-                    Arrays.asList(new MessageAction(places.get(i).getName(), places.get(i).getStatus().toString())));
-            carusels.add(temp);
+  public TemplateMessage getRestaurant(double lat, double lng) {
+    String apiKey = "AIzaSyB6t-XO4BEyDh1jBzHmeZn5hVB0WQkZLe8";
+    GooglePlaces client = new GooglePlaces(apiKey);
+    List<Place> places =
+        client.getNearbyPlaces(
+            lat,
+            lng,
+            1000,
+            Param.name("type").value("restaurant"),
+            Param.name("language").value("zh-TW"));
+    List<CarouselColumn> carusels = new ArrayList<CarouselColumn>();
+    String photoUrl =
+        "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&key="
+            + apiKey
+            + "&photoreference=";
+    for (int i = 1; i < 6; i++) {
+      String imageUrl = photoUrl;
+      for (Photo p : places.get(i).getPhotos()) {
+        imageUrl = imageUrl + p.getReference();
+        System.out.println(places.get(i).getPhotos().size());
+        if (StringUtils.isEmpty(p.getReference())) {
+          continue;
         }
-        TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", new CarouselTemplate(carusels));
-        return templateMessage;
-    }
+        break;
+      }
 
+      CarouselColumn temp =
+          new CarouselColumn(
+              imageUrl,
+              Double.toString(places.get(i).getRating()),
+              places.get(i).getVicinity(),
+              Arrays.asList(
+                  new MessageAction(
+                      places.get(i).getName(), places.get(i).getStatus().toString())));
+      carusels.add(temp);
+    }
+    TemplateMessage templateMessage =
+        new TemplateMessage("Carousel alt text", new CarouselTemplate(carusels));
+    return templateMessage;
+  }
 }
