3db331c41a708d6c203c96cbaf3afde009aa16b6
brianlin <brianlin>
2017-12-18 14:59:58 +0800

    Calendar - test

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.bot.spring.service.CalendarService;
 import com.example.bot.spring.service.HoroscopeService;
 import com.example.bot.spring.service.PttService;
 import com.example.bot.spring.service.RestaurantService;
@@ -33,116 +34,106 @@ import lombok.NonNull;
 @LineMessageHandler
 public class EchoApplication {
 
-    private String channelToken = "OD6ub5Qyystuid9ouEmNPBRLFmQTyeAbEX9ngG3WB9Scma4cDIM5qanrZ5dmJgnoKPxGwMQlsyDC8jm3p7LMLinTKRJDuMBrJ4ACM0egQIppZBoCGtCYA0rgBp8PSb8EkJppGlP0BhaWFVaeiyQddwdB04t89";
+  private String channelToken =
+      "OD6ub5Qyystuid9ouEmNPBRLFmQTyeAbEX9ngG3WB9Scma4cDIM5qanrZ5dmJgnoKPxGwMQlsyDC8jm3p7LMLinTKRJDuMBrJ4ACM0egQIppZBoCGtCYA0rgBp8PSb8EkJppGlP0BhaWFVaeiyQddwdB04t89";
 
-    @Autowired
-    private LineMessagingClient lineMessagingClient;
+  @Autowired private LineMessagingClient lineMessagingClient;
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(EchoApplication.class, args);
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(EchoApplication.class, args);
+  }
+
+  @EventMapping
+  public void handleLocationMessageEvent(MessageEvent<LocationMessageContent> event)
+      throws Exception {
+    List<Message> Messages = new ArrayList<Message>();
+    Source source = event.getSource();
+    String pushId = source.getUserId();
+    if (source instanceof GroupSource) {
+      pushId = ((GroupSource) source).getGroupId();
+    } else if (source instanceof RoomSource) {
+      pushId = ((RoomSource) source).getRoomId();
+    }
+    TemplateMessage message =
+        getRestaurant(event.getMessage().getLatitude(), event.getMessage().getLongitude());
+    Messages.add(message);
+    push(channelToken, pushId, Messages);
+  }
+
+  @EventMapping
+  public void handleDefaultMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
+    Source source = event.getSource();
+    List<Message> Messages = new ArrayList<Message>();
+    String pushId = source.getUserId();
+    if (source instanceof GroupSource) {
+      pushId = ((GroupSource) source).getGroupId();
+    } else if (source instanceof RoomSource) {
+      pushId = ((RoomSource) source).getRoomId();
     }
 
-    @EventMapping
-    public void handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) throws Exception {
-        List<Message> Messages = new ArrayList<Message>();
-        Source source = event.getSource();
-        String pushId = source.getUserId();
-        if (source instanceof GroupSource) {
-            pushId = ((GroupSource)source).getGroupId();
-        }
-        else if (source instanceof RoomSource) {
-            pushId = ((RoomSource)source).getRoomId();
-        }
-        TemplateMessage message = getRestaurant(event.getMessage().getLatitude(), event.getMessage().getLongitude());
-        Messages.add(message);
-        push(channelToken, pushId, Messages);
+    String message = ((TextMessageContent) event.getMessage()).getText();
+    String keyworad = message.substring(1);
+    if (message.startsWith("%")) {
+      Messages.add(PTT(keyworad));
+    } else if (message.startsWith("#")) {
+      Messages.add(Horoscope(keyworad));
+    } else if (message.startsWith("&")) {
+      Messages.addAll(getYoutube(keyworad));
+    } else if (message.startsWith("$")) {
+      Messages.add(Weather(keyworad));
+    } else if (message.startsWith("-c")) {
+      Messages.addAll(getCalendar());
+    } else if (message.equals("Botbye")) {
+      if (source instanceof GroupSource) {
+        lineMessagingClient.leaveGroup(((GroupSource) source).getGroupId()).get();
+      } else if (source instanceof RoomSource) {
+        lineMessagingClient.leaveRoom(((RoomSource) source).getRoomId()).get();
+      }
+      return;
     }
-
-    @EventMapping
-    public void handleDefaultMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
-        Source source = event.getSource();
-        List<Message> Messages = new ArrayList<Message>();
-        String pushId = source.getUserId();
-        if (source instanceof GroupSource) {
-            pushId = ((GroupSource)source).getGroupId();
-        }
-        else if (source instanceof RoomSource) {
-            pushId = ((RoomSource)source).getRoomId();
-        }
-
-        String message = ((TextMessageContent)event.getMessage()).getText();
-        String keyworad = message.substring(1);
-        if (message.startsWith("%")) {
-            Messages.add(PTT(keyworad));
-        }
-        else if (message.startsWith("#")) {
-            Messages.add(Horoscope(keyworad));
-        }
-        else if (message.startsWith("&")) {
-            Messages.addAll(getYoutube(keyworad));
-        }
-        else if (message.startsWith("$")) {
-            Messages.add(Weather(keyworad));
-        }
-        else if (message.equals("Botbye")) {
-            if (source instanceof GroupSource) {
-                lineMessagingClient.leaveGroup(((GroupSource)source).getGroupId()).get();
-            }
-            else if (source instanceof RoomSource) {
-                lineMessagingClient.leaveRoom(((RoomSource)source).getRoomId()).get();
-            }
-            return;
-        }
-        if (Messages.isEmpty()) {
-            return;
-        }
-        push(channelToken, pushId, Messages);
-
+    if (Messages.isEmpty()) {
+      return;
     }
+    push(channelToken, pushId, Messages);
+  }
 
-    public static TextMessage PTT(String message) {
-        String result = PttService.getInstance().getPttMessage(message);
-        return new TextMessage(result);
+  public static TextMessage PTT(String message) {
+    String result = PttService.getInstance().getPttMessage(message);
+    return new TextMessage(result);
+  }
+
+  public static TextMessage Weather(String message) {
+    String result = WeatherService.getInstance().getWeather(message);
+    return new TextMessage(result);
+  }
+
+  public static TextMessage Horoscope(String message) {
+    String result = HoroscopeService.getInstance().getHoroscope(message);
+    return new TextMessage(result);
+  }
+
+  public static List<Message> getYoutube(String message) throws Exception {
+    List<Message> result = YoutubeService.getInstance().getYoutube(message);
+    return result;
+  }
+
+  public static List<Message> getCalendar() throws Exception {
+    List<Message> result = CalendarService.getInstance().getCalendar();
+    return result;
+  }
+
+  public static TemplateMessage getRestaurant(double lat, double lng) throws Exception {
+    TemplateMessage result = RestaurantService.getInstance().getRestaurant(lat, lng);
+    return result;
+  }
+
+  private void push(
+      @NonNull String channelToken, @NonNull String pushId, @NonNull List<Message> messages)
+      throws Exception {
+    try {
+      lineMessagingClient.pushMessage(new PushMessage(pushId, messages)).get();
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
     }
-
-    public static TextMessage Weather(String message) {
-        String result = WeatherService.getInstance().getWeather(message);
-        return new TextMessage(result);
-    }
-
-    public static TextMessage Horoscope(String message) {
-        String result = HoroscopeService.getInstance().getHoroscope(message);
-        return new TextMessage(result);
-    }
-
-    public static List<Message> getYoutube(String message) throws Exception {
-        List<Message> result = YoutubeService.getInstance().getYoutube(message);
-        return result;
-    }
-
-    public static TemplateMessage getRestaurant(double lat, double lng) throws Exception {
-        TemplateMessage result = RestaurantService.getInstance().getRestaurant(lat, lng);
-        return result;
-    }
-
-    private void push(@NonNull String channelToken, @NonNull String pushId, @NonNull List<Message> messages)
-            throws Exception {
-        try {
-            lineMessagingClient.pushMessage(new PushMessage(pushId, messages)).get();
-        }
-        catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void push2(@NonNull String channelToken, @NonNull String pushId, @NonNull TemplateMessage message)
-            throws Exception {
-        try {
-            lineMessagingClient.pushMessage(new PushMessage(pushId, message)).get();
-        }
-        catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+  }
 }
---
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/CalendarService.java
@@ -0,0 +1,139 @@
+package com.example.bot.spring.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+
+public class CalendarService {
+  /** Application name. */
+  private final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+
+  /** Directory to store user credentials for this application. */
+  private static final java.io.File DATA_STORE_DIR =
+      new java.io.File(System.getProperty("user.home"), ".credentials/calendar-java-quickstart");
+
+  /** Global instance of the {@link FileDataStoreFactory}. */
+  private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+  /** Global instance of the JSON factory. */
+  private final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+  /** Global instance of the HTTP transport. */
+  private static HttpTransport HTTP_TRANSPORT;
+
+  private static CalendarService instance = new CalendarService();
+
+  public static CalendarService getInstance() {
+    return instance;
+  }
+
+  public CalendarService() {}
+
+  /**
+   * Global instance of the scopes required by this quickstart.
+   *
+   * <p>If modifying these scopes, delete your previously saved credentials at
+   * ~/.credentials/calendar-java-quickstart
+   */
+  private final List<String> SCOPES = Arrays.asList(CalendarScopes.CALENDAR_READONLY);
+
+  static {
+    try {
+      HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+      DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+    } catch (Throwable t) {
+      t.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+  /**
+   * Creates an authorized Credential object.
+   *
+   * @return an authorized Credential object.
+   * @throws IOException
+   */
+  public Credential authorize() throws IOException {
+    // Load client secrets.
+    InputStream in = CalendarService.class.getResourceAsStream("/client_secret.json");
+    GoogleClientSecrets clientSecrets =
+        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+    // Build flow and trigger user authorization request.
+    GoogleAuthorizationCodeFlow flow =
+        new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+            .setDataStoreFactory(DATA_STORE_FACTORY)
+            .setAccessType("offline")
+            .build();
+    Credential credential =
+        new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+    return credential;
+  }
+
+  /**
+   * Build and return an authorized Calendar client service.
+   *
+   * @return an authorized Calendar client service
+   * @throws IOException
+   */
+  public Calendar getCalendarService() throws IOException {
+    Credential credential = authorize();
+    return new com.google.api.services.calendar.Calendar.Builder(
+            HTTP_TRANSPORT, JSON_FACTORY, credential)
+        .setApplicationName(APPLICATION_NAME)
+        .build();
+  }
+
+  public List<Message> getCalendar() throws IOException {
+    List<Message> messages = new ArrayList<>();
+    Calendar service = getCalendarService();
+
+    // List the next 10 events from the primary calendar.
+    DateTime now = new DateTime(System.currentTimeMillis());
+    Events events =
+        service
+            .events()
+            .list("primary")
+            .setMaxResults(10)
+            .setTimeMin(now)
+            .setOrderBy("startTime")
+            .setSingleEvents(true)
+            .execute();
+    List<Event> items = events.getItems();
+    if (items.size() == 0) {
+      messages.add(new TextMessage("沒事"));
+    } else {
+      messages.add(new TextMessage("接下來"));
+      for (Event event : items) {
+        DateTime start = event.getStart().getDateTime();
+        if (start == null) {
+          start = event.getStart().getDate();
+        }
+        messages.add(new TextMessage(event.getSummary() + " (" + start + ")\n"));
+      }
+    }
+    return messages;
+  }
+}
