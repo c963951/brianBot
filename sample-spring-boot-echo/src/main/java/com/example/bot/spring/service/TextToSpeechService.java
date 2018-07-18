4a57866f4871b749ae55d6396618ea0479eaede5
brianlin <brianlin@intumit.com>
2018-07-18 15:16:15 +0800

    tts - 8

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/TextToSpeechService.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/service/TextToSpeechService.java
@@ -11,6 +11,12 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.bot.spring.echo.EchoApplication;
@@ -31,20 +37,20 @@ public class TextToSpeechService {
 
     public TextToSpeechService() {}
 
-    public AudioMessage getTTs(String word) {
+    public AudioMessage getTTs(String word) throws LineUnavailableException, UnsupportedAudioFileException {
         log.info("word(*******************"+word);
         try {
             word = java.net.URLEncoder.encode(word, "UTF-8");
             URL url = new URL("https://translate.google.com/translate_tts?ie=UTF-8&tl=zh-tw&client=tw-ob&q=" + word);
             HttpURLConnection urlConn = (HttpURLConnection)url.openConnection();
             urlConn.addRequestProperty("User-Agent", "Mozilla/4.76");
-            InputStream audioSrc = urlConn.getInputStream();
+            AudioInputStream audioSrc = AudioSystem.getAudioInputStream(url);
             log.info("audioSrc=========="+audioSrc.read());
             System.out.println("audioSrc=========="+audioSrc.read());
-            DownloadedContent mp4 = saveContent("m4a", audioSrc);
-            log.info("mp4url--------------"+mp4.getUri());
-            System.out.println("mp4url--------------"+mp4.getUri());
-            return new AudioMessage("https://translate.google.com/translate_tts?ie=UTF-8&tl=zh-tw&client=tw-ob&q=" + word, 1000);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioSrc);
+            Double d = clip.getMicrosecondLength() / 1000000D;
+            return new AudioMessage("https://translate.google.com/translate_tts?ie=UTF-8&tl=zh-tw&client=tw-ob&q=" + word, d.intValue());
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
